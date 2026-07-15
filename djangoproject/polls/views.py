f77518d7dc62c7d6c9a0401d2af537ee37faca6f
SANGAREY <156669546+SANGAREY@users.noreply.github.com>
2026-07-15 08:58:28 +0000

    Add files via upload

---
+++ djangoproject/polls/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Question, Choice
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.views import generic
+from django.utils import timezone
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_text"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+    context_object_name = "question"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+    context_object_name = "question"
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    template_name = "polls/detail.html"
+    context = {"error_message":"You didn't a select a choice", "question":question}
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, template_name, context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
